a0d36dd4fdfa7c967587acae5695a8955d64b3cb
Arjun Nair <arjunnair.mec@gmail.com>
2024-04-28 14:20:00 +0200

    Restructure files; Test file loading for trie

--- test/test_trie.py
+++ test/test_trie.py
@@ -1,14 +1,24 @@
 import unittest
-import trie
+from trie import Trie
 
 class TestTrie(unittest.TestCase):
 
     def test_multiple_words(self):
-        t = trie.Trie()
+        t = Trie()
         t.insert('apple')
         t.insert('banana')
         t.insert('orange')
         t.insert('app')
+        self.assertTrue(t['a']['p']['p']['l']['e'].end)
+        self.assertTrue(t['b']['a']['n']['a']['n']['a'].end)
+        self.assertTrue(t['o']['r']['a']['n']['g']['e'].end)
+        self.assertTrue(t['a']['p']['p'].end)
+        self.assertFalse(t['a']['p']['p']['l'].end)
+        self.assertFalse(t['z'].end)
+
+    def test_read_file(self):
+        t = Trie()
+        t.read_file('test/test_words.txt')
         self.assertTrue(t['a']['p']['p']['l']['e'].end)
         self.assertTrue(t['b']['a']['n']['a']['n']['a'].end)
         self.assertTrue(t['o']['r']['a']['n']['g']['e'].end)
